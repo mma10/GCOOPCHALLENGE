d939e22e0453ef8077b952a69a3ac33702a326f7
Md Mudassir Ali <mma10@iitbbs.ac.in>
2022-03-28 22:44:37 +0530

    Initialized class and relationships between entities

--- gymkhana.cpp
+++ gymkhana.cpp
@@ -10,37 +10,76 @@ class gymkhana{
     private:
         // Initial budget given by the government
         int budget;
-        
-        // Numerics of the gymkhana
-        vector<int> limitPosition = {1, 1, INT_MAX,INT_MAX,INT_MAX,INT_MAX};
-        vector<int> countPosition = {0, 0, 0, 0, 0, 0};
-        
-        // Hierarchy tree
+
+        // responsibility class
         class responsibility{
-            public:
-                // Value of the node
-                string position;
-                string designation;
-                student inchargeStudent;
-                
-                // Connections of the node
-                responsibility* parent;
-                vector<responsibility*> children;
+            protected:                              
+                int position;
+                string designation;                              
+        };    
+
+        // Initialize societyMembers class
+        class SocietyMember: private responsibility{
+            private:
+                student member;
         };
-        responsibility* root;
+
+        // Initialize society class
+        class Society: private responsibility{
+            private:
+                student secretary;                
+                vector<SocietyMember>  members;
+                int budget;                                
+        };
+
+        // Inititalize Council class
+        class Council: private responsibility{
+            private:
+                student secretary;
+                vector<Society> societies;
+        };
+
+        // Initialize VicePresident class
+        class VicePresident: private responsibility{
+            private:
+                student vicePresident;
+                vector<Council> councils;
+                // + extra roles
+        };    
         
+        // Initialize President class
+        class President: private responsibility{
+            // president + gymkhana
+            private:
+                student president;
+                VicePresident* vicePresident;
+                // + extra roles
+        };
+
+        President president;
+        VicePresident vicePresident;
+        vector<Council> councils;
+        vector<Society> societies;
+        vector<SocietyMember> societyMembers;
+    
         
     public:
-        vector<string> position;
+        vector<string> positions;
         void addResposibility(){
             
         } 
         
+        // gymkhana constructor
         gymkhana(){
-            position = {"President", "VicePresident", "GSecCouncil", "SecSociety", "MemSociety", "RemStudent"};
-            root = NULL;
+            positions = {"President", "VicePresident", "GSecCouncil", "SecSociety", "MemSociety", "RemStudent"};
+                                                         
         }
-        
+
+        // limit is the max % of budget alloted to each society
+        // alloted is the % of budget requested by each society
+        bool allocateBudget(vector<int> limit,vector<int> alloted){
+            
+        }        
 };
 
 void greet(){
